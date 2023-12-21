16a6ad92ec43736c290f8fd5b8727f7b63cfcdd0
Janie <janiebeasmith@gmail.com>
2023-12-21 17:27:45 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace OTRS_Counting_Variations
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.combinationLabel = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(536, 78);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(87, 27);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
+            this.textBox1.Location = new System.Drawing.Point(192, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(316, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(164, 194);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(484, 190);
+            this.splitContainer1.SplitterDistance = 308;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // combinationLabel
+            // 
+            this.combinationLabel.AutoSize = true;
+            this.combinationLabel.Location = new System.Drawing.Point(291, 171);
+            this.combinationLabel.Name = "combinationLabel";
+            this.combinationLabel.Size = new System.Drawing.Size(95, 20);
+            this.combinationLabel.TabIndex = 3;
+            this.combinationLabel.Text = "Combination";
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Location = new System.Drawing.Point(516, 171);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(89, 20);
+            this.countLabel.TabIndex = 4;
+            this.countLabel.Text = "Times Eaten";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.combinationLabel);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.browseButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button browseButton;
+        private TextBox textBox1;
+        private SplitContainer splitContainer1;
+        private Label combinationLabel;
+        private Label countLabel;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,21 @@
+namespace OTRS_Counting_Variations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "JSON |*.json";
+            
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fileDialog.FileName;
+            }
+        }
+    }
+}
